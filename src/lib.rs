385b9a679dec347909f96a629e6d2f0c575a3ac1
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-07-29 16:36:37 +1000

    [src/lib.rs] Traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+trait Transport: Send + Receive + Connection {
+    
+}
+
+trait Send: Ping {
+    fn send(connection: Connection, data: Vec<u8>) -> Result<u16 /* bytes sent */, Error>;
+}
+
+
+trait Receive: Ping {
+    fn receive(connection: Connection, data: Vec<u8>) -> Result<u16 /* bytes received */, Error>;
+}
+
+trait Ping {
+    fn ping(connection: Connection) -> Result<(), Error>;
+}
+
+trait Connection {
+    type Address;
+
+    fn connect(address: Address) -> Result<(), Error>;
+    fn disconnect(address: Address) -> Result<(), Error>;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
