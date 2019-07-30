646b34db016b09b486715db7d1f4e3ab2487ed11
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-07-30 16:36:25 +1000

    move Address and Data types into generics

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,34 @@ use std::sync::mpsc::Sender;
 
 // Transport trait for various implementations of message
 // sending/receiving services
-pub trait Transport {
+//
+// Address - external address type; e.g. PeerId for Consensus.
+// Translation into internal addresses is done in the implementation
+// based on configuration supplied, or based on implementation's internal data
+//
+// Data - Transmitting data type;
+// it can be a truct containing message type and payload data
+pub trait Transport<Address, Data>
+where
+    Data: AsRef<u8>,
+{
     // transport configuration type
     type Configuration;
-    // external address type; e.g. PeerId for Consensus.
-    // Translation into internal addresses is done in the implementation
-    // based on configuration supplied, or based on implementation's internal data
-    type Address;
-    // Transmitting data type; it can be a truct containing message type and payload data
-    type Data: AsRef<u8>;
 
     // Create new Transport instance
     fn new(cfg: Self::Configuration) -> Self;
 
-    // send specified message type to the specified peer
-    fn send(&mut self, peer: &Self::Address, data: Self::Data) -> Result<()>;
+    // send specified message to the specified peer
+    fn send(&mut self, peer: &Address, data: Data) -> Result<()>;
 
-    // broadcast specified message type to all peers
-    fn broadcast(&mut self, data: Self::Data) -> Result<()>;
+    // broadcast specified message to all peers
+    fn broadcast(&mut self, data: Data) -> Result<()>;
 
     // register a sending-half of std::sync::mpsc::channel which is used to push
     // all received messages to.
     // Several channels can be registered, they will be pushed in
     // the order of registration.
-    fn register_channel(&mut self, sender: Sender<Self::Data>) -> Result<()>;
+    fn register_channel(&mut self, sender: Sender<Data>) -> Result<()>;
 }
 
 mod errors;
