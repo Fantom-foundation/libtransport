0135ccb843465aebb40c84d0cdb7025a0211c87b
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-07-30 15:48:36 +1000

    draft of Transport trait; for consensus-raft

---
+++ src/errors.rs
@@ -0,0 +1,16 @@
+use fantom_common_rs::errors::Error as BaseError;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    Base(BaseError),
+    AtMaxVecCapacity,
+}
+
+impl From<BaseError> for Error {
+    #[inline]
+    fn from(be: BaseError) -> Error {
+        Error::Base(be)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,35 @@
-trait Transport: Send + Receive + Connection {
-    
-}
+use crate::errors::Result;
+use std::sync::mpsc::Sender;
 
-trait Send: Ping {
-    fn send(connection: Connection, data: Vec<u8>) -> Result<u16 /* bytes sent */, Error>;
-}
-
-
-trait Receive: Ping {
-    fn receive(connection: Connection, data: Vec<u8>) -> Result<u16 /* bytes received */, Error>;
-}
-
-trait Ping {
-    fn ping(connection: Connection) -> Result<(), Error>;
-}
-
-trait Connection {
+// Transport trait for various implementations of message
+// sending/receiving services
+pub trait Transport {
+    // transport configuration type
+    type Configuration;
+    // external address type; e.g. PeerId for Consensus.
+    // Translation into internal addresses is done in the implementation
+    // based on configuration supplied, or based on implementation's internal data
     type Address;
+    // Transmitting data type; it can be a truct containing message type and payload data
+    type Data: AsRef<u8>;
 
-    fn connect(address: Address) -> Result<(), Error>;
-    fn disconnect(address: Address) -> Result<(), Error>;
+    // Create new Transport instance
+    fn new(cfg: Self::Configuration) -> Self;
+
+    // send specified message type to the specified peer
+    fn send(&mut self, peer: &Self::Address, data: Self::Data) -> Result<()>;
+
+    // broadcast specified message type to all peers
+    fn broadcast(&mut self, data: Self::Data) -> Result<()>;
+
+    // register a sending-half of std::sync::mpsc::channel which is used to push
+    // all received messages to.
+    // Several channels can be registered, they will be pushed in
+    // the order of registration.
+    fn register_channel(&mut self, sender: Sender<Self::Data>) -> Result<()>;
 }
+
+mod errors;
 
 #[cfg(test)]
 mod tests {
