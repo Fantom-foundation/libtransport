c9870222f4389e94b7351a9e943d4f230fb31cc2
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-08-02 16:23:43 +1000

    simplify parameter types of Transport trait

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,15 @@ use std::sync::mpsc::Sender;
 //
 // peer_address - network address of the peer; e.g. "IP:port".
 //
+// Id - peer ID type
 // Data - Transmitting data type;
+// Error - error type returned by methods of Pl: PeerList
 // it can be a truct containing message type and payload data
-pub trait Transport<Id, Peer, Data, Error, Pl>
+pub trait Transport<Id, Data, Error, Pl>
 where
     Data: AsRef<u8> + Serialize,
     Id: PeerId,
-    Pl: PeerList<Id, Error, Item = Peer>,
+    Pl: PeerList<Id, Error>,
 {
     // transport configuration type
     type Configuration;
